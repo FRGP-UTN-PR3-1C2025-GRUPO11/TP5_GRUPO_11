c4569112880869a6513a797cddfc8512cbc4f99d
Tomas <tomas.a.gallardo006@gmail.com>
2025-05-04 16:33:15 -0300

    Update Ej01EliminarSucursal.aspx.cs

--- TP5_Grupo_11/Ej01EliminarSucursal.aspx.cs
+++ TP5_Grupo_11/Ej01EliminarSucursal.aspx.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.SqlClient;
 namespace TP5_Grupo_11
 {
     public partial class Ej01EliminarSucursal : System.Web.UI.Page
     {
+        private string consultaSql;
+        private int filasAlt;
+        Conexion conexion = new Conexion();
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,7 +19,9 @@ namespace TP5_Grupo_11
 
         protected void btnEliminar_Click(object sender, EventArgs e)
         {
+            consultaSql = "DELETE FROM Sucursal WHERE Id_Sucursal=" + txtSucursal.Text;
 
+            filasAlt = conexion.ejecutarModificacion(consultaSql);
         }
     }
 }
